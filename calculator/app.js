0ef39dc79813f673f0c1d721b8df06b2f0ee05d6
AWilliamson88 <87967861+AWilliamson88@users.noreply.github.com>
2022-10-10 19:45:53 +0800

    Fix operators & improve sequential calculation

--- calculator/app.js
+++ calculator/app.js
@@ -2,35 +2,29 @@ const calcBtns = document.querySelector('.calc-pad');
 const upper = document.querySelector('.upper');
 const lower = document.querySelector('.lower');
 
-
-const calc = {
-    dispValue: '0',
-    firstInput: null,
-    waitForSecondInput: false,
-    operator: null,
-};
+// Main display value
+var dispValue = "0";
+// the current equation
+let equation = null;
 
 // Reset the display.
 const resetDisplay = () => {
-    lower.textContent = calc.dispValue;
-    upper.textContent = calc.firstInput;
+    lower.textContent = dispValue;
+    upper.textContent = "";
 }
 
 // Update the display
 const updateDisp = () => {
-    lower.textContent = calc.dispValue;
-    if (calc.firstInput !== null && calc.waitForSecondInput == true) {
-        upper.textContent += calc.firstInput;
-        upper.textContent += calc.operator;
-    }
+    lower.textContent = dispValue;
+    upper.textContent = equation;
 }
 updateDisp();
-
 
 // Add event listeners
 calcBtns.addEventListener('click', (event) => {
     const { target } = event;
-    if (!target.matches('button')) {
+
+    if (!target.matches('button') && !target.matches('i')) {
         return;
     }
     if (target.classList.contains('operator')) {
@@ -43,14 +37,19 @@ calcBtns.addEventListener('click', (event) => {
         updateDisp();
         return;
     }
-    if (target.classList.contains('all-clear')) {
-        allClear();
+    if (target.classList.contains('clear-all')) {
+        clearAll();
         resetDisplay();
         updateDisp();
         return;
     }
-    if (target.classList.contains('delete')) {
-        deleteOne();
+    if (target.classList.contains('clear-one')) {
+        clearOne();
+        updateDisp();
+        return;
+    }
+    if (target.classList.contains('equals')) {
+        calculate();
         updateDisp();
         return;
     }
@@ -58,74 +57,49 @@ calcBtns.addEventListener('click', (event) => {
     updateDisp();
 });
 
-
-
 // Update the screen with the numbers
 function inputNumber(number) {
-    const { dispValue, waitForSecondInput } = calc;
-    if (waitForSecondInput === true) {
-        calc.dispValue = number;
-        calc.waitForSecondInput = false;
+    if (dispValue === '0') {
+        // If the number is greater then 0 replace the display value.
+        dispValue = (number != 0) ? number : '0';
     } else {
-        if (dispValue === '0') {
-            // If the number is greater then 0 replace the display value.
-            calc.dispValue = (number > 0) ? number : '0';
-        } else {
-            // When the display value is not 0 concat the number to it.
-            calc.dispValue = dispValue + number;
-        }
+        // When the display value is not 0 concat the number to it.
+        dispValue = dispValue + number;
     }
-    console.log(calc);
 }
 
 function inputDecimal(val) {
     // Only If the `dispValue` does not contain a decimal point append the decimal
-    if (!calc.dispValue.includes(val)) {
-        calc.dispValue += val;
+    if (!dispValue.includes(val)) {
+        dispValue += val;
     }
 }
 
 function handleOperator(Oper) {
-    const { firstInput, dispValue, operator } = calc
-    const input = parseFloat(dispValue);
+    const operators = ["+", "-", "/", "*"]
 
-    if (firstInput === null) {
-        calc.firstInput = input;
-        calc.dispValue = 0;
-    } else if (operator) {
-
-        // Get the correct function or none if there's no match.
-        const func = performCalc[operator];
-        if (func) {
-            calc.dispValue = String(func(firstInput, input));
-            calc.firstInput = input;
-        } else {
-            // handle bad operator.
-            // EG: Throw an exception
-
-        }
+    let lastValue = String(dispValue).slice(-1);
+    if (operators.includes(lastValue)) {
+        dispValue = dispValue.slice(0, -1) + Oper;
+    } else {
+        dispValue += Oper;
     }
-    calc.waitForSecondInput = true;
-    calc.operator = Oper;
-    console.log(calc);
 }
 
-const deleteOne = () => {
-
+function calculate() {
+    equation = dispValue;
+    dispValue = eval(dispValue);
 }
 
-const allClear = () => {
-    calc.dispValue = '0';
-    calc.firstInput = null;
-    calc.waitForSecondInput = false;
-    calc.operator = null;
+function clearOne() {
+    dispValue = String(dispValue).slice(0, -1);
+
+    if (dispValue === "")
+        dispValue = "0";
 }
 
-const performCalc = {
-    '/': (firstInput, secondInput) => firstInput / secondInput,
-    '*': (firstInput, secondInput) => firstInput * secondInput,
-    '+': (firstInput, secondInput) => firstInput + secondInput,
-    '-': (firstInput, secondInput) => firstInput - secondInput,
-    '=': (firstInput, secondInput) => secondInput
-};
-
+function clearAll() {
+    dispValue = "0";
+    equation = null;
+    operator = null;
+}
