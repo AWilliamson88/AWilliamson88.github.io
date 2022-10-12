a8b467850492d0e5c4eeb7fa3329ef7a4c0c9c52
AWilliamson88 <87967861+AWilliamson88@users.noreply.github.com>
2022-10-12 22:00:30 +0800

    Refactor code for OOP

--- calculator/app.js
+++ calculator/app.js
@@ -2,21 +2,18 @@ const calcBtns = document.querySelector('.calc-pad');
 const upper = document.querySelector('.upper');
 const lower = document.querySelector('.lower');
 
-// Main display value
-var dispValue = "0";
-// the current equation
-let equation = null;
+const calc = new Calculator();
 
 // Reset the display.
 const resetDisplay = () => {
-    lower.textContent = dispValue;
+    lower.textContent = calc.dispValue;
     upper.textContent = "";
 }
 
 // Update the display
 const updateDisp = () => {
-    lower.textContent = dispValue;
-    upper.textContent = equation;
+    lower.textContent = calc.dispValue;
+    upper.textContent = calc.dispEquation;
 }
 updateDisp();
 
@@ -28,78 +25,31 @@ calcBtns.addEventListener('click', (event) => {
         return;
     }
     if (target.classList.contains('operator')) {
-        handleOperator(target.value);
+        calc.handleOperator(target.value);
         updateDisp();
         return;
     }
     if (target.classList.contains('decimal')) {
-        inputDecimal(target.value);
+        calc.inputDecimal(target.value);
         updateDisp();
         return;
     }
     if (target.classList.contains('clear-all')) {
-        clearAll();
+        calc.clearAll();
         resetDisplay();
         updateDisp();
         return;
     }
     if (target.classList.contains('clear-one')) {
-        clearOne();
+        calc.clearOne();
         updateDisp();
         return;
     }
     if (target.classList.contains('equals')) {
-        calculate();
+        calc.calculate();
         updateDisp();
         return;
     }
-    inputNumber(target.value);
+    calc.inputNumber(target.value);
     updateDisp();
 });
-
-// Update the screen with the numbers
-function inputNumber(number) {
-    if (dispValue === '0') {
-        // If the number is greater then 0 replace the display value.
-        dispValue = (number != 0) ? number : '0';
-    } else {
-        // When the display value is not 0 concat the number to it.
-        dispValue = dispValue + number;
-    }
-}
-
-function inputDecimal(val) {
-    // Only If the `dispValue` does not contain a decimal point append the decimal
-    if (!dispValue.includes(val)) {
-        dispValue += val;
-    }
-}
-
-function handleOperator(Oper) {
-    const operators = ["+", "-", "/", "*"]
-
-    let lastValue = String(dispValue).slice(-1);
-    if (operators.includes(lastValue)) {
-        dispValue = dispValue.slice(0, -1) + Oper;
-    } else {
-        dispValue += Oper;
-    }
-}
-
-function calculate() {
-    equation = dispValue;
-    dispValue = eval(dispValue);
-}
-
-function clearOne() {
-    dispValue = String(dispValue).slice(0, -1);
-
-    if (dispValue === "")
-        dispValue = "0";
-}
-
-function clearAll() {
-    dispValue = "0";
-    equation = null;
-    operator = null;
-}
--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,10 +1,64 @@
+class Calculator {
+    #value = "0";
+    #equation = null;
+    calculator() {
 
+    }
 
+    get dispValue() {
+        return this.#value;
+    }
 
+    get DispEquation() {
+        return this.#equation;
+    }
 
-const calc = {
-    dispValue: '0',
-    firstInput: null,
-    waitForSecondInput: false,
-    operator: null,
-};
+    // Update the screen with the numbers
+    inputNumber(number) {
+        if (this.#value === '0') {
+            // If the number is greater then 0 replace the display value.
+            this.#value = (number != 0) ? number : '0';
+        } else {
+            // When the display value is not 0 concat the number to it.
+            this.#value = this.#value + number;
+        }
+    }
+
+    inputDecimal(val) {
+        // Only If the `value` does not contain a decimal point append the decimal
+        if (!String(this.#value).includes(val)) {
+            this.#value += val;
+        }
+    }
+
+    handleOperator(Oper) {
+        const operators = ["+", "-", "/", "*"]
+        let lastValue = String(this.#value).slice(-1);
+        if (operators.includes(lastValue)) {
+            this.#value = this.#value.slice(0, -1) + Oper;
+        } else {
+            this.#value += Oper;
+        }
+    }
+
+    calculate() {
+        this.#equation = this.#value;
+        try {
+            this.#value = eval(this.#value);
+        } catch {
+            this.#value = "error";
+        }
+    }
+
+    clearOne() {
+        this.#value = String(this.#value).slice(0, -1);
+        if (this.#value === "")
+            this.#value = "0";
+    }
+
+    clearAll() {
+        this.#value = "0";
+        this.#equation = null;
+    }
+
+}
