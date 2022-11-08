57258ea813f7c847e555171a4ca805043ff61ad0
AWilliamson88 <87967861+AWilliamson88@users.noreply.github.com>
2022-11-08 10:11:00 +0800

    Markdown previewer
    Can choose between markdown previewer or html.

---
+++ MarkdownPreviewer/app.js
@@ -0,0 +1,37 @@
+const editorContent = document.getElementById('editor');
+const previewContent = document.getElementById('preview');
+const htmlContent = document.getElementById('html');
+const displayTypes = document.getElementById('displayTypes');
+let currentdisplay;
+
+const updatePreview = async () => {
+
+    let editorText = await editorContent.value;
+    let previewText = await marked.parse(editorText);
+
+    currentdisplay.innerHTML = previewText;
+    // previewContent.innerHTML = previewText;
+    // htmlContent.innerHTML = previewText;
+}
+
+const updateDisplay = (e) => {
+    // Get the display type that was selected.
+    const display = document.getElementById(e);
+
+    // remove show class from all displays.
+    let panes = document.querySelectorAll('.pane');
+    panes.forEach(p => p.classList.remove('show'));
+
+    // Update and show the current display.
+    display.classList.add('show');
+    currentdisplay = display;
+    updatePreview();
+}
+
+editorContent.addEventListener('input', updatePreview);
+displayTypes.addEventListener('change', e => {
+    updateDisplay(e.target.value)
+});
+
+updatePreview();
+updateDisplay("preview");
